cf8ac951130ab5d00d6d93ff26643ea9fd91d55e
Revc Ra <x15967311210@gmail.com>
2023-03-19 16:11:33 +0800

    优化归并排序，通过交换数组和辅助数组的角色来，避免 MERGE 操作中两个数组之间的元素复制

--- P_1908_逆序对.cpp
+++ P_1908_逆序对.cpp
@@ -1,18 +1,16 @@
 // https://www.luogu.com.cn/problem/P1908
 
 #include <iostream>
+#include <algorithm>
 
 typedef long long ll;
 
 int a1[5'000'00];
 int a2[5'000'00];
 
-int *cur = a1;
-int *oth = a2;
-
 int n;
 
-ll merge(int begin, int end)
+ll merge(int *arr, int *aux, int begin, int end)
 {
     ll cnt = 0;
     int mid = (begin + end) / 2;
@@ -20,52 +18,42 @@ ll merge(int begin, int end)
     int i = begin, j = mid, k = begin;
     while (i < mid && j < end)
     {
-        if (cur[i] <= cur[j])
+        if (aux[i] <= aux[j])
         {
-            oth[k++] = cur[i++];
+            arr[k++] = aux[i++];
         }
         else
         {
-            oth[k++] = cur[j++];
+            arr[k++] = aux[j++];
             cnt += mid-i;
         }
     }
 
     while(i < mid)
     {
-        oth[k++] = cur[i++];
+        arr[k++] = aux[i++];
     }
     while (j < end)
     {
-        oth[k++] = cur[j++];
-    }
-
-    if (end == n) // avoid to copy elements
-    {
-        int *tmp = oth;
-        oth      = cur;
-        cur      = tmp;
-    }
-    else for (int i = begin; i < k; i++)
-    {
-        cur[i] = oth[i];
+        arr[k++] = aux[j++];
     }
 
     return cnt;
 }
 
-ll count_inversion(int begin, int end)
+ll count_inversion(int *arr, int *aux, int begin, int end)
 {
     if (end - begin <= 1) return 0;
     int mid = (begin + end) / 2;
-    ll left  = count_inversion(begin, mid);
-    ll right = count_inversion(mid, end);
+    ll left  = count_inversion(aux, arr, begin, mid); // sort the first half
+    ll right = count_inversion(aux, arr, mid, end);   // sort the second half
 
-    if (begin < mid && cur[mid-1] <= cur[mid]) // already sorted
+    if (begin < mid && aux[mid-1] <= aux[mid]) // already sorted
     {
+        std::copy(aux+begin, aux+end, arr+begin);
         return left + right;
     }
-    return left + right + merge(begin, end);
+    return left + right + merge(arr, aux, begin, end);
 }
 
 int main()
@@ -77,11 +65,10 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        std::cin >> cur[i];
-        oth[i] = cur[i];
+        std::cin >> a1[i];
+        a2[i] = a1[i];
     }
 
-
-    std::cout << count_inversion(0, n) << '\n';
+    std::cout << count_inversion(a1, a2, 0, n) << '\n';
     return 0;
 }
